2a910676ce496fb0ef6a41fc513431d973ce12b8
hyokkkk <tahyok7123@naver.com>
2020-11-04 21:11:37 +0900

    original + comment

--- decode-test.s
+++ decode-test.s
@@ -20,23 +20,23 @@
 
 	.globl	test
 test:
-	.word	test0
-        .word   test10
-	.word	test1
-	.word	test2
-	.word	test3
 	.word	test4
 	.word	test5
+#       .word   test10
+	.word	test3
+	.word	test2
+	.word	test0
+	.word	test1
 
 	.globl	ans
 ans:
-	.word	ans0
-        .word   ans10
-	.word	ans1
-	.word	ans2
-	.word	ans3
 	.word	ans4
 	.word	ans5
+#.word   ans10
+	.word	ans3
+	.word	ans2
+	.word	ans0
+	.word	ans1
 
 #self-made
 
